a819abc793346394824e13a698b60a6ddc547f8e
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2024-07-27 19:35:51 +0200

    rendering list

--- src/zimowe1.jsx
+++ src/zimowe1.jsx
@@ -6,58 +6,35 @@ import { useEffect, useState } from 'react';
 const baseURL = 'http://localhost/school1//wp-json/wp/v2/wyjazd_zimowy'
 export default function InfoZimowe() {
 
-    const [post, setPost] = useState([]);
+    const [post, setPost] = useState(null);
 
     useEffect(() => {
         async function getInfo() {
             await axios.get(baseURL).then((response) => {
                 setPost(response.data);
-                //console.log(response.data)
-
+                console.log(response.data)
             })
                 .catch(function (error) {
                     // handle error
                     console.log(error);
                 })
-                .then(function () {
+                .finally(function () {
                     // always executed
-                    //console.log(post)
                 });
         }
         getInfo()
-
-
     }, [])
-
-
-    // const listItems = post.map(product =>
-    //     <li key={product.id}>
-    //         {product.title}
-    //     </li>
-    // );
-
-    //console.log(post)
-    const listItems = post.map(product =>
-        <li key={product.id}>
-            <p> {product.id} </p>
-            <p> {product.title.rendered}</p>
-            <p> {product.acf.opis}</p>
-            <div dangerouslySetInnerHTML={{ __html: product.acf.opis_extra }} />
-        </li>
-    );
-
     //console.log(post)
     if (!post) return null;
     return (
-
         <div>
-            {/* <h1>{post[0].id}</h1>
+            <h1>{post[0].id}</h1>
             <p>{post[0].title.rendered}</p>
             <p>{post[0].acf.opis}</p>
             <p>opublikowano: {post[0].date}</p>
-            <div dangerouslySetInnerHTML={{ __html: post[0].acf.opis_extra }} /> */}
-            test
-            <ul>{listItems}</ul>
+            <div dangerouslySetInnerHTML={{ __html: post[0].acf.opis_extra }} />
+
+
         </div>
     );
 
